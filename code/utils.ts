e8399dd560bb0c2ead0279c53969b34e61e4f520
BL <1201678-bandleader@users.noreply.gitlab.com>
2023-12-19 19:37:02 -0500

    add `mishnaByIndex` function

--- code/utils.ts
+++ code/utils.ts
@@ -1,4 +1,5 @@
 // numberToGematria(716) === "תשיז"
+// numberToGematria(285) === "ערה" (one of a few traditional exceptions, to avoid unpleasant words)
 function numberToGematria(num: number) {
   const inner = (x: number) =>
     x > 400 ? "ת" + inner(x - 400) :
@@ -9,4 +10,14 @@ function numberToGematria(num: number) {
     !x ? "" :
     " אבגדהוזחט"[x]
   return num === 270 ? "ער" : num === 272 ? "ערב" : num === 275 ? "ערה" : inner(num)
+}
+
+// Returns perek & mishna numbers for a given mishna index.
+// Example: mishnaByIndex(7, [7, 5]) === [0, 7] === פרק א משנה ז
+// Example: mishnaByIndex(8, [7, 5]) === [1, 0] === פרק ב משנה א
+// If the index is out of bounds, returns null.
+function mishnaByIndex(zeroBasedInd: number, perekLengths: number[]): [number, number] | null {
+  return zeroBasedInd < perekLengths[0] ? [0, zeroBasedInd] :
+    perekLengths.length <= 1 ? null :
+    mishnaByIndex(zeroBasedInd - perekLengths[0], perekLengths.slice(1))
 }
