542e441642058cf19fc9c245d292c634bd07ddb0
BL <1201678-bandleader@users.noreply.gitlab.com>
2024-01-07 21:30:23 -0500

    Add gematria calculator to utils.ts

--- code/utils.ts
+++ code/utils.ts
@@ -21,3 +21,37 @@ function mishnaByIndex(zeroBasedInd: number, perekLengths: number[], _carry = 0)
     perekLengths.length <= 1 ? null :
     mishnaByIndex(zeroBasedInd - perekLengths[0], perekLengths.slice(1), _carry + 1)
 }
+
+// Simple gematria calculator
+const gematriaMap = {
+  'א': 1,
+  'ב': 2,
+  'ג': 3,
+  'ד': 4,
+  'ה': 5,
+  'ו': 6,
+  'ז': 7,
+  'ח': 8,
+  'ט': 9,
+  'י': 10,
+  'כ': 20,
+  'ך': 20,
+  'ל': 30,
+  'מ': 40,
+  'ם': 40,
+  'נ': 50,
+  'ן': 50,
+  'ס': 60,
+  'ע': 70,
+  'פ': 80,
+  'ף': 80,
+  'צ': 90,
+  'ץ': 90,
+  'ק': 100,
+  'ר': 200,
+  'ש': 300,
+  'ת': 400,
+}
+function gematria(str: string) {
+  return str.split('').reduce((acc, char) => acc + (gematriaMap[char] || 0), 0)
+}
